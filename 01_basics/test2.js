a72a70d79c03494b51bd865610c45df9dd3faa51
Adin Shahab <shahabadin25@outlook.com>
2024-03-30 20:54:50 +0000

    test2 stack and heap

--- 01_basics/test2.js
+++ 01_basics/test2.js
@@ -34,7 +34,33 @@ let str2="adin";
 let str3=str1+str2;
 //console.log(str3);
 
-console.log("1"+2);
-console.log(1+"2");
-console.log("1"+2+2);
-console.log(1+2+"2");
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+
+
+///*************************** */
+//memory is of 2 types
+//stack -- primitive(copy of value)
+//heap --non primitive(reference of the value)
+//in heap changes are done in original value
+// let myYoutubeName="adin shahab";//primitive 
+
+// let anotherName=myYoutubeName;
+// anotherName="chaiaurCode";
+// console.log(myYoutubeName);
+// console.log(anotherName);
+
+let user={
+    email:"user@gmail.com",
+    upi:"123"
+}
+let userTwo=user;
+userTwo.email="adin@gmail.com";
+console.log(user.email);
+console.log(userTwo.email);
+
+
+
