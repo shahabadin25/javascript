6b977b3ca68fed3f3a9eb9bc0b9f7ca720d6165b
Adin Shahab <shahabadin25@outlook.com>
2024-03-30 19:56:48 +0000

    type conversion

---
+++ 01_basics/test2.js
@@ -0,0 +1,23 @@
+// let score=null;
+//  console.log(typeof(score));
+
+//  let valueInNumber=Number(score);
+//  console.log(typeof valueInNumber);
+//  console.log(valueInNumber);
+
+
+ //number is converted to number
+ //number + string is converted in NaN
+ //true=>1
+ 
+ let isLoggedIn=1;
+ let booleanIsLoggedIn=Boolean(isLoggedIn);
+ console.log(booleanIsLoggedIn);
+
+ //1 =>true
+ //""=> false
+ //"hitesh=>true"
+
+ let someNumber=33;
+ let stringNumber=String(someNumber);
+ console.log(typeof stringNumber);
