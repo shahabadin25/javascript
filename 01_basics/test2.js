b3484e256cda33636fe6c0fdf31114d5e0ce5fa3
Adin Shahab <shahabadin25@outlook.com>
2024-03-30 20:26:53 +0000

    test2

--- 01_basics/test2.js
+++ 01_basics/test2.js
@@ -10,14 +10,31 @@
  //number + string is converted in NaN
  //true=>1
  
- let isLoggedIn=1;
- let booleanIsLoggedIn=Boolean(isLoggedIn);
- console.log(booleanIsLoggedIn);
+//  let isLoggedIn=1;
+//  let booleanIsLoggedIn=Boolean(isLoggedIn);
+//  console.log(booleanIsLoggedIn);
 
  //1 =>true
  //""=> false
  //"hitesh=>true"
 
- let someNumber=33;
- let stringNumber=String(someNumber);
- console.log(typeof stringNumber);
+//  let someNumber=33;
+//  let stringNumber=String(someNumber);
+//  console.log(typeof stringNumber);
+
+ //****************operations********** */
+let value=3;
+let negValue= -value;
+// console.log(negValue);
+
+// console.log(2**3);//2^3;
+
+let str1="hello";
+let str2="adin";
+let str3=str1+str2;
+//console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
