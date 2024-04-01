50c20d166ecc60cf3a9d62ca3066b34e909e0ce5
Adin Shahab <shahabadin25@outlook.com>
2024-04-01 19:40:55 +0000

    date

---
+++ 01_basics/date.js
@@ -0,0 +1,29 @@
+//let myDate=new Date();
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleDateString());
+// console.log(typeof myDate);
+
+
+//let myCreateDate=new Date(2023,0,23);//month in js starts from 0
+//let myCreatedDate=new Date("01-14-2023");//month in js starts from 0
+//console.log(myCreateDate.toLocaleString());
+
+
+
+// let myTimeStamp=Date.now();
+// console.log(myTimeStamp); //vlaues in ms
+// console.log(myCreatedDate.getTime()); //values in ms
+// console.log(Math.floor(Date.now()/1000));//floor takes the smaller value
+
+
+
+let newDate=new Date();
+console.log(newDate);
+console.log(newDate.getMonth());
+console.log(newDate.getDay());
+
+newDate.toLocaleString('Default'{
+    weekday:"long",
+    
+})
