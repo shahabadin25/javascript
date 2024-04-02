7855581c2d8c001e80f2f81fe1d1aebf1be122ea
Adin Shahab <shahabadin25@outlook.com>
2024-04-02 19:49:48 +0000

    arrays

--- 02_basics/arrays.js
+++ 02_basics/arrays.js
@@ -27,12 +27,48 @@ const myArr2=new Array(1,2,3,4);
 
 //slice and splice
 //in slice original array is not manipulated
-console.log("A ",myArr);
-const myn1=myArr.slice(1,3);//slice displays on those values which are present in the given range of index
-console.log(myn1);
-console.log("B ",myArr);
-//in splice original array is manipulated,in original array it removes the slice portion
-const myn2=myArr.splice(1,3);
-console.log("C ",myArr);
-console.log(myn2);
+// console.log("A ",myArr);
+// const myn1=myArr.slice(1,3);//slice displays on those values which are present in the given range of index
+// console.log(myn1);
+// console.log("B ",myArr);
+// //in splice original array is manipulated,in original array it removes the slice portion
+// const myn2=myArr.splice(1,3);
+// console.log("C ",myArr);
+// console.log(myn2);
 
+const marvel_heros=["thor","ironman","spiderman"]
+const dc_heros=["superman","flash","batman"]
+
+// marvel_heros.push(dc_heros);
+// console.log(marvel_heros);
+
+//so access flash 
+//console.log(marvel_heros[3][1]);
+
+
+//concat merges 2 arrays and returns a new arrar
+const allHeros=marvel_heros.concat(dc_heros);
+console.log(allHeros);
+
+//spread operator is better to use
+const all_new_heros=[...marvel_heros,...dc_heros];//this is called spread operator
+console.log(all_new_heros);
+
+const another_array=[1,2,3,[4,5,6],7,[6,7,[4,5]]];
+const real_another_array=another_array.flat(Infinity); //flat() merges all the arrarys into arrays into  a single array
+//Infinity is the depth upto to which u want to combine the arrays into arrays into a single array ... it can be a number too
+
+console.log(real_another_array);
+
+
+
+console.log(Array.isArray("Adin"));//isArray() checks whether it is an array or not
+console.log(Array.from("Adin"));//converts into an array
+console.log(Array.from({name: "adin"}));
+
+
+let score1=100
+let score2=200
+let score3=300
+
+console.log(Array.of(score1,score2,score3));//returns e new array from the set of elements.
