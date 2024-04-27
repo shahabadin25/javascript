06b03f6a000f2fa1950fcec385fdac480c9a9678
Adin Shahab <shahabadin25@outlook.com>
2024-04-27 15:44:36 +0000

    filter maps

---
+++ 03_basics/filterMap.js
@@ -0,0 +1,24 @@
+//const coding=["js","ruby","java","python","cpp"]
+
+// const values=coding.forEach((item)=>{
+//     console.log(item);
+//     return item;
+// }) //forEach loop doesnt return any values
+// console.log(values);
+
+
+const myNums=[1,2,3,4,5,6,7,8,9,10];
+// const newNums=myNums.filter((num)=> num>4)
+// //filter returns values
+// //if we open a scope that is {} then to return a value we must write the return keyword
+// console.log(newNums);
+
+
+// const newNums=[];
+// myNums.forEach((nums)=>{
+//     if(nums>4){
+//         newNums.push(nums);
+//     }
+// })
+
+// console.log(newNums);
